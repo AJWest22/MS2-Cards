75168fcb5df5ba5274c070a307b60b0fc55f7009
AJWest22 <abiwestcott@gmail.com>
2022-03-12 14:40:14 +0000

    added function to flip a card

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const cards = document.getElementsByClassName('memory-card')
 
 cards.forEach(card => card.addEventListener('click'));
+
+function cardFlip() {
+    this.classList.toggle('flip');
+}
