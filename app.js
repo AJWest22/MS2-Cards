63e24acf005c98c405297313f706b9593d46502d
AJWest22 <abiwestcott@gmail.com>
2022-03-12 15:13:02 +0000

    corrected js file so card now flips

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const cards = document.querySelectorAll('.memory-card')
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.memory-card')
 
-function cardFlip() {
-    cards.classList.toggle("cardFlip");
-}
+    function cardFlip() {
+        this.classList.toggle('cardFlip');
+    }
 
-cards.forEach(card => card.addEventListener("click", cardFlip));
+    cards.forEach(card => card.addEventListener('click', cardFlip));
+
+})
