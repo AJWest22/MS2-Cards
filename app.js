50d973f71d0905737fcfe00f49b197aa09241799
AJWest22 <abiwestcott@gmail.com>
2022-07-29 14:04:12 +0000

    Update site layout

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    //javascript for slideshow
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 5000);
+    }
+
     const cards = document.querySelectorAll('.memory-card');
     let flippedCard = false;
     let lockCards = false;
